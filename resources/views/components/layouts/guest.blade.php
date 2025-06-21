913faff90fe3ed433075e7d28655e7068c73e95b
sulthanalif <sulthanalif45@gmail.com>
2025-06-22 00:04:40 +0700

    feat(layouts): add favicon links and update logout tooltip text
    Add favicon links for various devices to both guest and app layouts
    Update logout button tooltip text from "logoff" to "Logout" for consistency

--- resources/views/components/layouts/guest.blade.php
+++ resources/views/components/layouts/guest.blade.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('logo.png') }}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('logo.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('logo.png') }}">
     <title>{{ isset($title) ? $title.' - '.config('app.name') : config('app.name') }}</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
